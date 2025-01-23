ae5772012d803647f6bcf0d67dc0cb20df6a5fee
Yash Suthar <yashsuthar0309@gmail.com>
2025-01-24 03:28:07 +0530

    setup backend and routes for the daily tasks #9

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const cors = require('cors');
+const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
-PORT = process.env.PORT || 5000;
+app.use(cors());
+PORT = process.env.PORT || 5001;
+
+app.use("/api/dailytask", require("./routes/dailyTaskRoutes"));
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
